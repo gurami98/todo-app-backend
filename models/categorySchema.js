ba640116fc641351f1b594e8f0a84c386ec56788
gurami-exceed <g.chilindrishvili.exceedteam@gmail.com>
2021-08-31 17:06:42 +0400

    changed category scheme to reference user

--- models/categorySchema.js
+++ models/categorySchema.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose')
+const {Schema} = require("mongoose");
 
 const categorySchema = new mongoose.Schema({
     categories: {
         type: Array,
         required: true
     },
-    user: {
-        type: String,
-        required: true
-    }
+    user: {type: Schema.Types.String, ref: 'user'}
 })
 
 const categoryModel = mongoose.model('category', categorySchema)
